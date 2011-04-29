ee54eb97b3131659552a3019ce4c0a43d39cc0fb
BertaS <BertaS@35766a92-c723-e34b-82d8-7ce82bbb31b5>
2011-04-29 14:40:42 +0000

    Working test application with simple sprite drawing

---
+++ OkuEngine/trunk/OkuTest/ParticleTestGame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OkuEngine;
+
+namespace OkuTest
+{
+  public class ParticleTestGame : OkuGame
+  {
+    ImageContent _smiley = null;
+    float rotation = 0.0f;
+
+    public override void Initialize()
+    {
+      _smiley = new ImageContent(".\\content\\yinyang.png");
+    }
+
+    public override void Update(float dt)
+    {
+      rotation += dt * 360;
+    }
+
+    public override void Render()
+    {
+      OkuDrivers.Renderer.DrawImage(_smiley, new Vector(100, 100), rotation);
+    }
+
+  }
+}
