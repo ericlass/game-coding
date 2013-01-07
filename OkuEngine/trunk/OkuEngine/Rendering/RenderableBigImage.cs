a76e20d00796c3bc621c9411b40fda5e34d1a6d2
bertas <bertas@35766a92-c723-e34b-82d8-7ce82bbb31b5>
2013-01-07 15:24:20 +0000

    - Added big image renderable

---
+++ OkuEngine/trunk/OkuEngine/Rendering/RenderableBigImage.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+
+namespace OkuEngine.Rendering
+{
+  /// <summary>
+  /// Description of RenderableBigImage.
+  /// </summary>
+  public class RenderableBigImage : IRenderable
+  {
+    public RenderableBigImage()
+    {
+    }
+    
+    public void Update(float dt)
+    {
+      throw new NotImplementedException();
+    }
+      
+    public void Render(OkuEngine.Scenes.Scene scene)
+    {
+      throw new NotImplementedException();
+    }
+      
+    public AABB GetBoundingBox()
+    {
+      throw new NotImplementedException();
+    }
+      
+    public bool Load(System.Xml.XmlNode node)
+    {
+      throw new NotImplementedException();
+    }
+      
+    public bool Save(System.Xml.XmlWriter writer)
+    {
+      throw new NotImplementedException();
+    }
+  
+  }
+}
