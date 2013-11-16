18a2d4c0a1267744f63c9970759c9b81836da8fc
bertas <bertas@35766a92-c723-e34b-82d8-7ce82bbb31b5>
2013-11-16 15:04:00 +0000

    - New player controller with controller base - New player sprites with simple animations

--- Rouge/RougeLike/ControllerProcess.cs
+++ Rouge/RougeLike/ControllerProcess.cs
@@ -22,6 +22,32 @@ namespace RougeLike
     {      
     }
 
+    private Orientation VectorToOrientation(Vector2f vec)
+    {
+      const double quartertPi = Math.PI * 0.25;
+      const double threeQurterPi = Math.PI * 0.75;
+
+      //Vector2f other = Vector2f.Normalize(vec);
+      double angle = Math.Atan2(vec.Y, vec.X);
+      double angleAbs = Math.Abs(angle);
+
+      if (angleAbs >= 0.0 && angleAbs <= quartertPi)
+      {
+        return Orientation.Right;
+      }
+      else if (angleAbs >= threeQurterPi && angleAbs <= Math.PI)
+      {
+        return Orientation.Left;
+      }
+      else
+      {
+        if (angle > 0)
+          return Orientation.Up;
+        else
+          return Orientation.Down;
+      }
+    }
+
     public abstract bool IsMoving(Entity entity, ref Vector2f movement);
     public abstract bool IsAttacking(Entity entity);
 
@@ -29,20 +55,36 @@ namespace RougeLike
     {
       foreach (Entity entity in Entities)
 	    {
-        //Move
-        Vector2f movement = Vector2f.Zero;
-        if (IsMoving(entity, ref movement))
+        if (entity.StateMachine.CurrentStateId.EndsWith("idle"))
         {
-          TransformComponent trans = entity.GetComponent<TransformComponent>(TransformComponent.ComponentId);
-          if (trans == null)
-            throw new InvalidOperationException("Entity " + entity.Id + " has no transform component! It must have one for the controller to be able to handle it!");
-
-          trans.Translation += movement;
-
-          //TODO: Set orientation regarding the movement vector
+          Vector2f movement = Vector2f.Zero;
+          if (IsMoving(entity, ref movement))
+          {
+            Orientation direction = VectorToOrientation(movement);
+            entity.StateMachine.CurrentStateId = direction.ToString().ToLower() + "_move";
+          }
         }
+        else if (entity.StateMachine.CurrentStateId.EndsWith("move"))
+        {
+          Vector2f movement = Vector2f.Zero;
+          if (IsMoving(entity, ref movement))
+          {
+            Orientation direction = VectorToOrientation(movement);
+            entity.StateMachine.CurrentStateId = direction.ToString().ToLower() + "_move";
 
-        if (IsAttacking(entity))
+            TransformComponent trans = entity.GetComponent<TransformComponent>(TransformComponent.ComponentId);
+            if (trans == null)
+              throw new InvalidOperationException("Entity " + entity.Id + " has no transform component!");
+
+            trans.Translation += movement * dt;
+          }
+          else
+          {
+            string[] parts = entity.StateMachine.CurrentStateId.Split('_');
+            entity.StateMachine.CurrentStateId = parts[0] + "_idle";
+          }
+        }
+        else if (entity.StateMachine.CurrentStateId.EndsWith("attack"))
         {
 
         }
