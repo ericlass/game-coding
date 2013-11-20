072aca52b775a13e17b305f7c1472a0f0d9ae406
bertas <bertas@35766a92-c723-e34b-82d8-7ce82bbb31b5>
2013-11-20 17:45:34 +0000

    - Started weapons

---
+++ trunk/Rouge/RougeLike/IWeapon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RougeLike
+{
+  public interface IWeapon
+  {
+    string ItemId { get; }
+    WeaponType Type { get; }
+    int BaseDamage { get; }
+    void Render();
+  }
+}
