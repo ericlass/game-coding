061e434edd9a3bf208fa4ba4bf2d6bb63b4cda0c
Administrator <Administrator@35766a92-c723-e34b-82d8-7ce82bbb31b5>
2014-01-02 14:27:44 +0000

    States in player object

---
+++ Rouge/RougeLike/DoubleKeyMap.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collection.Generic;
+
+namespace RougeLike
+{
+  public class DoubleKeyMap<K1, K2, V>
+  {
+    private Dictionary<K1, Dictionary<K2, V>> _map = new Dictionary<K1, Dictionary<K2, V>>();
+    
+    public DoubleKeyMap()
+    {
+    }
+    
+    public bool Contains(K1 key1)
+    {
+      return _map.ContainsKey(key1);
+    }
+    
+    public bool Contains(K1 key1, K2 key2)
+    {
+      if (!Contains(key1))
+        return false;
+        
+      return _map[key1].ContainsKey(key2);
+    }
+    
+    public void Add(K1 key1, K2 key2, V value)
+    {
+      if (Contains(key1, key2))
+        throw new Exception("Map already contains a value for the key pair " + key1 + " + " + key2 + "!");
+        
+      _map.Add(key1, new Dictionary<K2, V>());
+      _map[key1].Add(key2, value);
+    }
+    
+    public bool Remove(K1 key1, K2 key2)
+    {
+      if (!Contains(key1, key2))
+        return false;
+      
+      _map[key1].Remove(key2);
+      
+      //If map is empty, remove it too
+      if (_map[key1].Count == 0)
+        _map.Remove(key1);
+      
+      return true;
+    }
+    
+    public V this[K1 key1, K2 key2]
+    {
+      get
+      {
+        if (!Contains(key1, key2))
+          throw new Exception("Map does not contain a value for the key pair " + key1 + " + " + key2 + "!");
+          
+        return _map[key1][key2];
+      }
+      set
+      {
+        if (!Contains(key1, key2))
+          throw new Exception("Map does not contain a value for the key pair " + key1 + " + " + key2 + "!");
+          
+        _map[key1][key2] = value;
+      }
+    }
+    
+  }
+}
