fcbec6b5a958f81fdf4eeb33aaf3f38991c56ad2
bertas <bertas@35766a92-c723-e34b-82d8-7ce82bbb31b5>
2014-01-05 17:25:14 +0000

    - Added XML doc generation in release build - Added missing XML docs

--- trunk/JSONator/JSONator/JSONValueType.cs
+++ trunk/JSONator/JSONator/JSONValueType.cs
@@ -7,11 +7,29 @@ namespace JSONator
   /// </summary>
   public enum JSONValueType
   {
+    /// <summary>
+    /// Defines a null value.
+    /// </summary>
     Null,
+    /// <summary>
+    /// Defines a boolean value with true and false.
+    /// </summary>
     Bool,
+    /// <summary>
+    /// Defines a number value. Can be float or integer.
+    /// </summary>
     Number,
+    /// <summary>
+    /// Defines a string value.
+    /// </summary>
     String,
+    /// <summary>
+    /// Defines an object value.
+    /// </summary>
     Object,
+    /// <summary>
+    /// Defines an array value.
+    /// </summary>
     Array
   }
 }
