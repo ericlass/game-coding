1da7969a17cf182f08afbba69fc8dc1698430885
bertas <bertas@35766a92-c723-e34b-82d8-7ce82bbb31b5>
2014-02-10 18:54:04 +0000

    Generating level by noise

--- Rouge/RougeLike/WorldMapObject.cs
+++ Rouge/RougeLike/WorldMapObject.cs
@@ -12,6 +12,31 @@ namespace RougeLike
       get { return "worldmap"; }
     }
 
+    public override void Init()
+    {
+      _tiles = new Tile[100, 40];
+
+      for (int y = 0; y < _tiles.GetLength(1); y++)
+      {
+        for (int x = 0; x < _tiles.GetLength(0); x++)
+        {
+          _tiles[x, y] = new Tile(true, 0);
+        }
+      }
+      _tileImages = GameUtil.LoadSpriteSheet("simple_tiles.png", 16, 16);
+
+      PerlinNoise noise = new PerlinNoise();
+      for (int x = 0; x < _tiles.GetLength(0); x++)
+      {
+        int floor = (int)(noise.Noise(x, 0, 3, 50) * 10.0f) + (_tiles.GetLength(1) / 2);
+        for (int y = 0; y < floor; y++)
+        {
+          _tiles[x, y].TileIndex = 2;
+          _tiles[x, y].Walkable = false;
+        }
+      }
+    }
+
     public override void Update(float dt)
     {
     }
@@ -25,9 +50,7 @@ namespace RougeLike
       return new StringPairMap();
     }
 
-    public override void Init()
-    {
-    }
+    
 
   }
 }
