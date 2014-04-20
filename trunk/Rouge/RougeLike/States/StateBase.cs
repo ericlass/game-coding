56d2927323d46dd9ff9c10cb071e88140aa65867
bertas <bertas@35766a92-c723-e34b-82d8-7ce82bbb31b5>
2014-04-20 17:53:22 +0000

    - Started state machines for entities

---
+++ trunk/Rouge/RougeLike/States/StateBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RougeLike.States
+{
+  public abstract class StateBase
+  {
+    public abstract string Id { get; }
+
+    public abstract void Enter();
+    public abstract void Update(float dt);
+    public abstract void Render();
+    public abstract void Leave();
+  }
+}
---
+++ trunk/Rouge/RougeLike/States/StateMachine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RougeLike.States
+{
+  public class StateMachine
+  {
+    private Dictionary<string, StateBase> _states = null;
+    private List<Transition> _transitions = null;
+    private string _currentState = null;
+
+    public StateMachine()
+    {
+    }
+
+    //Continue: How is the state machine initialized. It has to know all states and transitions to register for the events. In the end they will be loaded from a file.
+
+    //How to get states to register themselves to the state factory? Maybe via reflections?
+
+  }
+}
