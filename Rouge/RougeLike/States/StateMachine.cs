d0f34dc16e6de8bd07970c376234981760c25dda
bertas <bertas@35766a92-c723-e34b-82d8-7ce82bbb31b5>
2014-04-28 17:11:14 +0000

    - Finished first version of state machine

--- Rouge/RougeLike/States/StateMachine.cs
+++ Rouge/RougeLike/States/StateMachine.cs
@@ -7,12 +7,113 @@ namespace RougeLike.States
 {
   public class StateMachine
   {
-    private Dictionary<string, StateBase> _states = null;
-    private List<Transition> _transitions = null;
+    private StateMap _states = new StateMap();
+    private List<Transition> _transitions = new List<Transition>();
+    private string _initialState = null;
+
+    private Dictionary<string, Transition> _transitionMap = null;
     private string _currentState = null;
+    private OnEventDelegate _eventListener = null;
+    private bool _initialized = false;
 
     public StateMachine()
     {
+    }
+
+    private OnEventDelegate GetEventListener()
+    {
+      if (_eventListener == null)
+        _eventListener = new OnEventDelegate(OnEvent);
+
+      return _eventListener;
+    }
+
+    public StateMap States
+    {
+      get { return _states; }
+    }
+
+    public List<Transition> Transitions
+    {
+      get { return _transitions; }
+    }
+
+    public string InitialState
+    {
+      get { return _initialState; }
+      set { _initialState = value; }
+    }
+
+    public string CurrentState
+    {
+      get { return _currentState; }
+      set
+      {
+        if (_currentState != null)
+          _states[_currentState].Leave();
+
+        if (!_states.ContainsKey(value))
+          throw new OkuBase.OkuException("State '" + value + "' is not available in this state machine!");
+
+        _currentState = value;
+        _states[_currentState].Enter();
+      }
+    }
+
+    public void Init()
+    {
+      if (_initialized)
+        throw new OkuBase.OkuException("Cannot initialize a state machine that has already been initialized!");
+
+      _currentState = _initialState;
+
+      //Not sure if should be done. If a state play a sound on entering, than the scene will make a lot of noise when it is loaded.
+      //if (_currentState != null)
+        //_states[_currentState].Enter();
+
+      _transitionMap = new Dictionary<string, Transition>();
+      foreach (Transition transition in _transitions)
+      {
+        GameData.Instance.EventQueue.AddListener(transition.EventId, GetEventListener());
+        _transitionMap.Add(transition.EventId, transition);
+      }
+
+      _initialized = true;
+    }
+
+    private void OnEvent(string eventId, object data)
+    {
+      if (!_initialized)
+        throw new OkuBase.OkuException("Event received in a non-initialized state machine!");
+
+      if (_transitionMap.ContainsKey(eventId))
+      {
+        _states[_currentState].Leave();
+        _currentState = _transitionMap[eventId].TargetState;
+        _states[_currentState].Enter();
+      }
+    }
+
+    public void Finish()
+    {
+      if (!_initialized)
+        throw new OkuBase.OkuException("Cannot finish a state machine that has not been initialized!");
+
+      foreach (Transition trans in _transitionMap.Values)
+        GameData.Instance.EventQueue.RemoveListener(trans.EventId, GetEventListener());
+
+      _transitionMap = null;
+
+      _initialized = false;
+    }
+
+    public void Clear()
+    {
+      if (_initialized)
+        throw new OkuBase.OkuException("Cannot clear a state machine that is already initialized! Finish it first!");
+
+      _states.Clear();
+      _transitions.Clear();
     }
 
     //Continue: How is the state machine initialized. It has to know all states and transitions to register for the events. In the end they will be loaded from a file.
---
+++ Rouge/RougeLike/States/StateMap.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RougeLike.States
+{
+  public class StateMap : Dictionary<string, StateBase>
+  {
+  }
+}
