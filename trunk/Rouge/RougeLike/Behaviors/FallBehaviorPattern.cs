30cbec56c1d2dbaf847ab4bdd6cba330b1561fa9
bertas <bertas@35766a92-c723-e34b-82d8-7ce82bbb31b5>
2014-06-17 04:45:40 +0000

    - Started behaviors

---
+++ trunk/Rouge/RougeLike/Behaviors/FallBehaviorPattern.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using OkuBase.Geometry;
+using RougeLike.Attributes;
+using RougeLike.Controller;
+using RougeLike.Objects;
+using RougeLike.States;
+
+namespace RougeLike.Behaviors
+{
+  public class FallBehaviorPattern : IBehaviorPattern
+  {
+    private float _speed = 0.0f;
+
+    public string Update(float dt, EntityObject entity, IEntityController controller)
+    {
+      _speed = Math.Min(_speed + (1500 * dt), 800);
+
+      float speedx = (float)entity.GetAttributeValue<NumberValue>("speedx").Value;
+
+      if (controller.DoMoveLeft(entity))
+        speedx -= 200 * dt;
+
+      if (controller.DoMoveRight(entity))
+        speedx += 200 * dt;
+
+      float maxSpeed = (float)entity.GetAttributeValue<NumberValue>("walkspeed").Value;
+      speedx = GameUtil.Clamp(speedx, -maxSpeed, maxSpeed);
+
+      entity.GetAttributeValue<NumberValue>("speedx").Value = speedx;
+
+      //Make sure entity is facing into the correct direction
+      if (speedx > 0)
+        entity.GetAttributeValue<NumberValue>("direction").Value = 1;
+      else if (speedx < 0)
+        entity.GetAttributeValue<NumberValue>("direction").Value = -1;
+
+      Vector2f movement = new Vector2f(speedx * dt, -_speed * dt);
+
+      TileMapObject tilemap = GameData.Instance.ActiveScene.GameObjects.GetObjectById("tilemap") as TileMapObject;
+      string result = null;
+
+      Rectangle2f hitbox = entity.GetTransformedHitBox();
+
+      float xBound = movement.X > 0 ? hitbox.Max.X : hitbox.Min.X;
+      Vector2f bottomPoint = new Vector2f(hitbox.GetCenter().X, hitbox.Min.Y);
+      Vector2f forwardPoint = new Vector2f(xBound, hitbox.GetCenter().Y);
+
+      Vector2f dv = movement;
+      Vector2f realMovement;
+      if (tilemap.CollideMovingPoint(bottomPoint, movement, out realMovement))
+      {
+        dv.Y = realMovement.Y;
+        result = WalkState.StateId;
+      }
+
+      if (tilemap.CollideMovingPoint(forwardPoint, movement, out realMovement))
+      {
+        dv.X = realMovement.X;
+        entity.GetAttributeValue<NumberValue>("speedx").Value = 0;
+      }
+
+      Vector2f pos = entity.Position;
+      pos += dv;
+      entity.Position = pos;
+
+      return result;
+    }
+
+    public void Begin(EntityObject entity)
+    {
+      _speed = 0.0f;
+    }
+
+    public void End(EntityObject entity)
+    {
+    }
+
+  }
+}
---
+++ trunk/Rouge/RougeLike/Behaviors/IBehaviorPattern.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using RougeLike.Objects;
+using RougeLike.Controller;
+
+namespace RougeLike.Behaviors
+{
+  public interface IBehaviorPattern
+  {
+    void Begin(EntityObject entity);
+    string Update(float dt, EntityObject entity, IEntityController controller);
+    void End(EntityObject entity);
+  }
+}
