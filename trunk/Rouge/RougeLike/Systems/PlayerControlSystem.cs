db822db08c466f452b543728ec62bf6df7327ac0
bertas <bertas@35766a92-c723-e34b-82d8-7ce82bbb31b5>
2014-07-10 08:28:53 +0000

    Safe commit before starting re-implementation of player control

---
+++ trunk/Rouge/RougeLike/Systems/PlayerControlSystem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using RougeLike.Objects;
+using RougeLike.Character;
+
+namespace RougeLike.Systems
+{
+  public class PlayerControlSystem : IGameSystem
+  {
+    private string _playerId = null; //ID of player object
+    private CharacterObject _playerObject = null;
+
+    public PlayerControlSystem(string playerId)
+    {
+      _playerId = playerId;
+    }
+
+    public string PlayerId
+    {
+      get { return _playerId; }
+      set { _playerId = value; }
+    }
+
+    public void Init()
+    {
+      _playerObject = GameData.Instance.ActiveScene.GameObjects.GetObjectById(_playerId) as CharacterObject;
+    }
+
+    public void Update(float dt)
+    {
+      switch (_playerObject.CurrentState)
+      {
+        case CharacterState.Idle:
+          break;
+        case CharacterState.Walking:
+          break;
+        case CharacterState.Jumping:
+          break;
+        case CharacterState.Falling:
+          break;
+        case CharacterState.Frozen:
+          break;
+        default:
+          break;
+      }
+    }
+
+    public void Finish()
+    {
+      _playerObject = null;
+    }
+
+  }
+}
