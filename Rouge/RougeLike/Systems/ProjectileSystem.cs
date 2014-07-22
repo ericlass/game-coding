cde89ea60f09b06763d2a9da65ae64bf4ac1e9cb
Administrator <Administrator@35766a92-c723-e34b-82d8-7ce82bbb31b5>
2014-07-22 11:55:16 +0000

    Started collision detection between projectiles and characters

--- Rouge/RougeLike/Systems/ProjectileSystem.cs
+++ Rouge/RougeLike/Systems/ProjectileSystem.cs
@@ -22,10 +22,102 @@ namespace RougeLike.Systems
     {
       //TODO: Implement
       // Get projectiles
-      // If no projectiles, return
+      List<GameObjectBase> projectilesObjects = GameData.Instance.ActiveScene.GameObjects.GetObjectsOfGroup(_projectileGroup);
+      if (projectilesObjects.Count = 0)
+        return;
+        
+      // Cast to projectiles
+      List<ProjectileObject> projectiles = new List<ProjectileObject>();
+      foreach (GameObjectBase obj in projectilesObjects)
+        projectiles.Add(obj as ProjectileObject);
+        
+      //Move projectiles
+      foreach (ProjectileObject proj in projectiles)
+        proj.Position = proj.Position + (proj.Direction * dt);
+            
       // Get targets
-      // calculate spatial hashes of projectiles and targets
+      List<GameObjectBase> characterObjects = GameData.Instance.ActiveScene.GameObjects.GetObjectsOfGroup(_hitGroup);
+      if (characterObjects.Count = 0)
+        return;
+        
+      // Cast to characters
+      List<CharacterObject> characters = new List<CharacterObject>();
+      foreach (GameObjectBase obj in characterObjects)
+        characters.Add(obj as CharacterObject);
+      
+      int gridSize = 64;
+      int projectileSize = 3; // TODO: Get this from weapon
+      
+      // calculate spatial hashes of projectiles
+      DoubleKeyMap<int, int, List<ProjectileObject>> projectileMap = new DoubleKeyMap<int, int, List<ProjectileObject>>();
+      foreach (ProjectileObject proj in projectiles)
+      {
+        int left = (proj.Position.X - projectileSize) / gridSize;
+        int right = (proj.Position.X + projectileSize) / gridSize;
+        int bottom = (proj.Position.Y - projectileSize) / gridSize;
+        int top = (proj.Position.Y + projectileSize) / gridSize;
+        
+        for (int x = left; x <= right; x++)
+        {
+          for (int = bottom; y <= top; y++)
+          {
+            if (!projectileMap.Contains(x, y))
+              projectileMap.Add(x, y, new List<ProjectileObject>());
+            
+            projectileMap[x, y].Add(proj);
+          }
+        }
+      }
+      
+      // calculate spatial hashes of targets
+      DoubleKeyMap<int, int, List<CharacterObject>> characterMap = new DoubleKeyMap<int, int, List<CharacterObject>>();
+      foreach (CharacterObject character in characters)
+      {
+        float halfWidth = character.HitBox.Width / 2.0f;
+        float halfHeight = character.HitBox.Height / 2.0f;
+        
+        int left = (character.Position.X - halfWidth) / gridSize;
+        int right = (character.Position.X + halfWidth) / gridSize;
+        int bottom = (character.Position.Y - halfHeight) / gridSize;
+        int top = (character.Position.Y + halfHeight) / gridSize;
+        
+        for (int x = left; x <= right; x++)
+        {
+          for (int = bottom; y <= top; y++)
+          {
+            if (!characterMap.Contains(x, y))
+              characterMap.Add(x, y, new List<CharacterObject>());
+            
+            characterMap[x, y].Add(proj);
+          }
+        }
+      }
+      
       // Check for collisions
+      List<KeyPair<int, int>> cells = projectileMap.GetKeys();
+      foreach (KeyPair<int, int> key in cells)
+      {
+        List<ProjectileObject> projs = projectileMap[key.Key1, key.Key2];
+        if (projs.Count = 0)
+          continue;
+          
+        if (!characterMap.Contains(key.Key1, key.Key2))
+          continue;
+          
+        List<CharacterObject> chars = characterMap[key.Key1, keyKey2];
+        if (chars.Count = 0)
+          continue;
+          
+        //Finally, check for collisions
+        foreach (ProjectileObject proj in projs)
+        {
+          foreach (CharacterObject chara in chars)
+          {
+            //TODO: Calculate collision
+          }
+        }
+      }
+      
       // if collission, apply damage
     }
 
