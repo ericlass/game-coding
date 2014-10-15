6629b9ed41ba55ebb6b5fedc351ef6e477068743
bertas <bertas@35766a92-c723-e34b-82d8-7ce82bbb31b5>
2014-10-15 17:09:00 +0000

    - Started ParticleGameSystem but not sure how it actually works

---
+++ trunk/Rouge/RougeLike/Systems/CollidingParticleController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using OkuBase.Geometry;
+using OkuBase.Particles;
+using RougeLike.Objects;
+
+namespace RougeLike.Systems
+{
+  public class CollidingParticleController : IParticleController
+  {
+    private string _tileMapId = null;
+
+    public CollidingParticleController(string tileMapId)
+    {
+      _tileMapId = tileMapId;
+    }
+
+    public void Update(Particle particle, float dt)
+    {
+      particle.OldPosition = particle.Position;
+
+      if (!particle.Velocity.IsZero())
+      {
+        TileMapObject tilemap = GameData.Instance.ActiveScene.GameObjects.GetObjectById(_tileMapId) as TileMapObject;
+        Vector2f maxMove;
+        if (tilemap.CollideMovingPoint(particle.Position, particle.Velocity * dt, out maxMove))
+        {
+          particle.Velocity = Vector2f.Zero;
+        }
+
+        particle.Position = particle.Position + maxMove * dt;
+      }
+
+      particle.Energy -= dt;
+    }
+
+  }
+}
