09d6038195b52a203f2b9f1df7ded9d35399b99c
bertas <bertas@35766a92-c723-e34b-82d8-7ce82bbb31b5>
2014-10-15 17:09:00 +0000

    - Started ParticleGameSystem but not sure how it actually works

---
+++ Rouge/RougeLike/Systems/ImageParticleRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using OkuBase;
+using OkuBase.Graphics;
+using OkuBase.Particles;
+
+namespace RougeLike.Systems
+{
+  public class ImageParticleRenderer : IParticleRenderer
+  {
+    private ImageBase _image = null;
+
+    public ImageParticleRenderer(ImageBase image)
+    {
+      _image = image;
+    }
+
+    public void Render(List<Particle> particles)
+    {
+      foreach (Particle p in particles)
+      {
+        OkuManager.Instance.Graphics.DrawImage(_image, p.Position.X, p.Position.Y, 0, p.Scale, p.Scale, p.Color);
+      }
+    }
+
+  }
+}
---
+++ Rouge/RougeLike/Systems/ParticleGameSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using OkuBase.Particles;
+
+namespace RougeLike.Systems
+{
+  public class ParticleGameSystem : IGameSystem
+  {
+    private ParticleSystem<LineEmitter, CollidingParticleController, ImageParticleRenderer> _particleSystem = null;
+
+    public void Init()
+    {
+      
+    }
+
+    public void Update(float dt)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void Finish()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
