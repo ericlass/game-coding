4ff9dce7beea9783647f4f8955db962c113c39d8
bertas <bertas@35766a92-c723-e34b-82d8-7ce82bbb31b5>
2015-01-29 18:58:50 +0000

    - Started implementation of rooms

---
+++ SimGame/SimGame/Game/Room.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimGame.Game
+{
+  public class Room
+  {
+    public RoomDefinition Definition { get; set; }
+    public int Level { get; set; }
+
+    public Room(RoomDefinition definition)
+    {
+      Definition = definition;
+    }
+  }
+}
---
+++ SimGame/SimGame/Game/RoomType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimGame.Game
+{
+  public enum RoomType
+  {
+    Empty,
+    Entrance,
+    Stairway,
+    Storage,
+    Facility,
+    Laboratory,
+    Security,
+    Administration
+  }
+}
