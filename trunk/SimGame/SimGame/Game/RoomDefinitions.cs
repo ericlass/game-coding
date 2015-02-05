0e289f348f8c93b261cef0305c4bd611f1a26f53
bertas <bertas@35766a92-c723-e34b-82d8-7ce82bbb31b5>
2015-02-05 17:19:25 +0000

    - Full building is now rendered

--- trunk/SimGame/SimGame/Game/RoomDefinitions.cs
+++ trunk/SimGame/SimGame/Game/RoomDefinitions.cs
@@ -1,24 +1,147 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
+using OkuBase.Graphics;
 
 namespace SimGame.Game
 {
   public static class RoomDefinitions
   {
-    public static RoomDefinition Empty = new RoomDefinition() { Id = "empty", Name = "Empty", BaseType = RoomType.Empty, Fixed = true, NumberOfSpaces = 1 };
+    public static RoomDefinition Empty = new RoomDefinition() 
+    {
+      Id = "empty",
+      Name = "Empty",
+      BaseType = RoomType.Empty,
+      Fixed = true,
+      NumberOfSpaces = 1,
+      BaseColor = new Color(128, 128, 128)
+    };
 
-    public static RoomDefinition Entrance = new RoomDefinition() { Id = "entrance", Name = "Entrance", BaseType = RoomType.Entrance, Fixed = true, NumberOfSpaces = 1 };
+    public static RoomDefinition Entrance = new RoomDefinition() 
+    { 
+      Id = "entrance", 
+      Name = "Entrance", 
+      BaseType = 
+      RoomType.Entrance, 
+      Fixed = true, 
+      NumberOfSpaces = 1,
+      BaseColor = Color.Green
+    };
 
-    public static RoomDefinition Stairway = new RoomDefinition() { Id = "stairs", Name = "Stairway", BaseType = RoomType.Stairway, Fixed = false, NumberOfSpaces = 1 };
+    public static RoomDefinition Stairway = new RoomDefinition() 
+    { 
+      Id = "stairs", 
+      Name = "Stairway", 
+      BaseType = RoomType.Stairway,
+      Fixed = false,
+      NumberOfSpaces = 1,
+      BaseColor = Color.Cyan
+    };
 
-    public static RoomDefinition LabSmall = new RoomDefinition() { Id = "labsmall", Name = "Small Lab", BaseType = RoomType.Laboratory, Fixed = false, NumberOfSpaces = 1 };
-    public static RoomDefinition LabMedium = new RoomDefinition() { Id = "labmedium", Name = "Medium Lab", BaseType = RoomType.Laboratory, Fixed = false, NumberOfSpaces = 2 };
-    public static RoomDefinition LabLarge = new RoomDefinition() { Id = "lablarge", Name = "Large Lab", BaseType = RoomType.Laboratory, Fixed = false, NumberOfSpaces = 3 };
+    public static RoomDefinition Administration = new RoomDefinition()
+    {
+      Id = "administration",
+      Name = "Administration",
+      BaseType = RoomType.Administration,
+      Fixed = false,
+      NumberOfSpaces = 1,
+      BaseColor = new Color(255, 128, 0)
+    };
 
-    public static RoomDefinition FacilitySmall = new RoomDefinition() { Id = "facilitysmall", Name = "Small Facility", BaseType = RoomType.Facility, Fixed = false, NumberOfSpaces = 1 };
-    public static RoomDefinition FacilityMedium = new RoomDefinition() { Id = "facilitymedium", Name = "Medium Facility", BaseType = RoomType.Facility, Fixed = false, NumberOfSpaces = 2 };
-    public static RoomDefinition FacilityLarge = new RoomDefinition() { Id = "facilitylarge", Name = "Large Facility", BaseType = RoomType.Facility, Fixed = false, NumberOfSpaces = 3 };
+    public static RoomDefinition Security = new RoomDefinition()
+    {
+      Id = "security",
+      Name = "Security",
+      BaseType = RoomType.Security,
+      Fixed = false,
+      NumberOfSpaces = 1,
+      BaseColor = Color.Red
+    };
+
+    public static RoomDefinition StorageSmall = new RoomDefinition()
+    {
+      Id = "storagesmall",
+      Name = "Small Storage",
+      BaseType = RoomType.Storage,
+      Fixed = false,
+      NumberOfSpaces = 1,
+      BaseColor = new Color(64, 64, 64)
+    };
+    public static RoomDefinition StorageMedium = new RoomDefinition()
+    {
+      Id = "storagemedium",
+      Name = "Medium Storage",
+      BaseType = RoomType.Storage,
+      Fixed = false,
+      NumberOfSpaces = 2,
+      BaseColor = new Color(64, 64, 64)
+    };
+    public static RoomDefinition StorageLarge = new RoomDefinition()
+    {
+      Id = "storagelarge",
+      Name = "Large Storage",
+      BaseType = RoomType.Storage,
+      Fixed = false,
+      NumberOfSpaces = 3,
+      BaseColor = new Color(64, 64, 64)
+    };
+
+    public static RoomDefinition LabSmall = new RoomDefinition() 
+    { 
+      Id = "labsmall", 
+      Name = "Small Lab", 
+      BaseType = 
+      RoomType.Laboratory, 
+      Fixed = false,
+      NumberOfSpaces = 1,
+      BaseColor = Color.Blue
+    };
+    public static RoomDefinition LabMedium = new RoomDefinition() 
+    { 
+      Id = "labmedium", 
+      Name = "Medium Lab", 
+      BaseType = RoomType.Laboratory, 
+      Fixed = false,
+      NumberOfSpaces = 2,
+      BaseColor = Color.Blue
+    };
+    public static RoomDefinition LabLarge = new RoomDefinition() 
+    { 
+      Id = "lablarge", 
+      Name = "Large Lab", 
+      BaseType = RoomType.Laboratory, 
+      Fixed = false,
+      NumberOfSpaces = 3,
+      BaseColor = Color.Blue
+    };
+
+    public static RoomDefinition FacilitySmall = new RoomDefinition() 
+    { 
+      Id = "facilitysmall", 
+      Name = "Small Facility", 
+      BaseType = RoomType.Facility, 
+      Fixed = false,
+      NumberOfSpaces = 1,
+      BaseColor = Color.Yellow
+    };
+    public static RoomDefinition FacilityMedium = new RoomDefinition() 
+    { 
+      Id = "facilitymedium", 
+      Name = "Medium Facility", 
+      BaseType = RoomType.Facility, 
+      Fixed = false,
+      NumberOfSpaces = 2,
+      BaseColor = Color.Yellow
+    };
+    public static RoomDefinition FacilityLarge = new RoomDefinition() 
+    { 
+      Id = "facilitylarge", 
+      Name = "Large Facility", 
+      BaseType = RoomType.Facility, 
+      Fixed = false,
+      NumberOfSpaces = 3,
+      BaseColor = Color.Yellow
+    };
 
     private static Dictionary<string, RoomDefinition> _definitions = null;
 
