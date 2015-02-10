49f699bfe9eee7111374ce60ae9c7b50c9ad3486
bertas <bertas@35766a92-c723-e34b-82d8-7ce82bbb31b5>
2015-02-10 18:26:41 +0000

    - Added experimantal base class for objects, not used yet

---
+++ trunk/SimGame/SimGame/Objects/GameObjectImpl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimGame.Objects
+{
+  public class GameObjectImpl
+  {
+    public GameObject Object { get; set; }
+
+    public virtual void Initialize() { }
+    public virtual void Update(float dt) { }
+    public virtual void Render() { }
+    public virtual void Finish() { }
+    public virtual void TriggerAction(string actionId, object[] parameters) { }
+  }
+}
